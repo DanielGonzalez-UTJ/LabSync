1d0735811e759401e48e74739e853f747078a4bd
DanielGonzalez-UTJ <2125100178@soy.utj.edu.mx>
2026-06-28 15:57:35 -0600

    Subida inicial del proyecto LabSync
    Se agrega el código fuente del sistema LabSync desarrollado en Java con NetBeans.

---
+++ src/main/java/labsync/labsync/LabSync.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package labsync.labsync;
+
+/**
+ *
+ * @author aleja
+ */
+public class LabSync {
+
+    public static void main(String[] args) {
+        Login ventanaLogin = new Login();
+        
+        ventanaLogin.setVisible(true);
+        ventanaLogin.setLocationRelativeTo(null);
+    }
+}
